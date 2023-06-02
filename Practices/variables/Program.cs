b0f4537f13bb11667603fda7fcaab18267e72513
bengugultekin <bengunurgultekin54@gmail.com>
2023-06-02 23:28:19 +0300

    variables homework

--- Practices/variables/Program.cs
+++ Practices/variables/Program.cs
@@ -4,8 +4,83 @@ class Program
 {
     static void Main(string[] args)
     {
-        int deger;
-        deger = 2;
-        Console.WriteLine(deger);
+        // int deger = 2;
+        // string degisken = null;
+        // string Degisken = null;
+        // string System = "";
+
+        Console.WriteLine("");
+
+        byte b = 5;     // 1 byte
+        sbyte c = 5;   // 1 byte
+
+        short s = 5;    // 2 byte
+        ushort us = 5;  // 2 byte
+
+        Int16 i16 = 2;  // 2 byte
+        int i = 2;      // 4 byte
+        Int32 i32 = 2;  // 4 byte
+        Int64 i64 = 2;  // 8 byte
+
+        uint ui = 2;
+        long l = 4;
+        ulong ul = 4;
+
+        // Reel Sayılar
+        float f = 5;    // 4 byte
+        double d =5;    // 8 byte
+        decimal de = 5; //16 byte
+    
+        char ch = 'k';           // 2 byte
+        string str = "asd";     // sınırsız
+
+        bool b1 = true;
+        bool b2 = false;
+
+        DateTime dt = DateTime.Now;
+        Console.WriteLine(dt); 
+
+        object o1 = "x";
+        object o2 = 'y';
+        object o3 = 4;
+        object o4 = 4.3;
+
+        // string ifadeler
+        string str1 = string.Empty;
+        str1 = "bengu gultekin";
+        string ad = "bengu";
+        string soyad = "gultekin";
+        string tamIsim = ad + " " + soyad;
+
+        // integer tanımlama şekilleri
+        int integer1 = 5;
+        int integer2 = 3;
+        int integer3 = integer1 * integer2;
+
+        // boolean
+        bool bool1 = 10>2; // true
+        bool bool2 = 10<2; // false
+
+        // Değişken dönüşümleri
+        string str20 = "20";
+        int int20 = 20;
+
+        string yeniDeger = str20 + int20.ToString();
+        Console.WriteLine(yeniDeger); // çıktısı 2020
+
+        int int21 = int20 + Convert.ToInt32(str20);
+        Console.WriteLine(int21); // çıktısı 40
+
+        int int22 = int20 + int.Parse(str20); // çıktısı 40
+
+        // Datetime
+        string datetime = DateTime.Now.ToString("dd.MM.yyyy");
+        Console.WriteLine(datetime);
+
+        string datetime2 = DateTime.Now.ToString("dd/MM/yyyy");
+        Console.WriteLine(datetime2);
+
+        string hour = DateTime.Now.ToString("HH:mm");
+        Console.WriteLine(hour);
     }
 }
