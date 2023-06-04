a7156121c4e2fb99c7aafbcb7ffa273938429751
bengugultekin <bengunurgultekin54@gmail.com>
2023-06-04 20:34:56 +0300

    loops for-loop

---
+++ Practices/loops-for-loop/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace loops_for_loop;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ekranda girilen sayıya kadar olan tek sayıları ekrana yazdırır.
+        Console.WriteLine("Lütfen bir sayı giriniz:");
+        int sayac = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= sayac; i++) {
+            if(i % 2 == 1)
+                Console.WriteLine(i);
+        }
+
+        // 1 ile 1000 arasındaki tek ve çift sayıların kendi içlerinde toplamlarını ekrana yazdırır.
+        int tekToplam = 0;
+        int ciftToplam = 0;
+        for (int i = 1; i <= 1000; i++) {
+            if(i % 2 == 1)
+                tekToplam += i;
+            else
+                ciftToplam += i;
+        }
+
+        Console.WriteLine("Tek Toplam: " + tekToplam);
+        Console.WriteLine("Çift Toplam: " + ciftToplam);
+
+        // break, contune
+        for (int i = 1; i < 10; i++)
+        {
+            if(i == 4)
+                break;
+            Console.WriteLine(i);
+        }
+        for (int i = 1; i < 10; i++)
+        {
+            if(i == 4)
+                continue;
+            Console.WriteLine(i);
+        }
+
+    }
+}
