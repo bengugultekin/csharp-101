6934504a376a5a5b9e853e7008e513667ee43375
bengugultekin <bengunurgultekin54@gmail.com>
2023-06-19 11:01:57 +0300

    homework 2

---
+++ Homeworks/equal-or-divisible/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace equal_or_divisible;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Lütfen bir sayı giriniz");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] numbers = new int[n];
+
+        for(int i=0; i < n; i++)
+        {
+            Console.WriteLine("Lütfen {0} sayisini giriniz:", i+1);
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Lütfen bir sayı daha giriniz");
+        int m = int.Parse(Console.ReadLine());
+
+        for(int i=0; i<n; i++)
+        {
+            if(numbers[i] == m || numbers[i] % m == 0)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+
+
+    }
+}
