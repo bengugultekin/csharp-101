53fa00811ac52aab9f9b32523ea8f7e35e294574
bengugultekin <bengunurgultekin54@gmail.com>
2023-06-19 11:27:01 +0300

    homework 4

---
+++ Homeworks/words-and-letters/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace words_and_letters;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int word = 1;
+        int letter = 0;
+        Console.WriteLine("Lütfen bir cümle yazın");
+        string sentence = Console.ReadLine();
+
+        
+        for(int i = 0; i<sentence.Length; i++)
+        {
+            if(sentence[i] != ' ')
+            {
+                letter++;
+            }
+            else if(sentence[i] == ' ')
+            {
+                word++;
+            }
+        }
+
+        Console.WriteLine("Cümledeki harf sayısı:" + letter);
+        Console.WriteLine("Cümledeki kelime sayısı:" + word);
+        
+    }
+}
