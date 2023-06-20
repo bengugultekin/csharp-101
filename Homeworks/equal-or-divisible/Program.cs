01ed12b17dc12f5ea660472b5921719f83fddc37
bengugultekin <bengunurgultekin54@gmail.com>
2023-06-20 21:59:06 +0300

    refreshed repo

--- Homeworks/equal-or-divisible/Program.cs
+++ Homeworks/equal-or-divisible/Program.cs
@@ -5,28 +5,40 @@ class Program
     static void Main(string[] args)
     {
         
-        Console.WriteLine("Lütfen bir sayı giriniz");
-        int n = int.Parse(Console.ReadLine());
+//         Console.WriteLine("Lütfen bir sayı giriniz");
+//         int n = int.Parse(Console.ReadLine());
 
-        int[] numbers = new int[n];
+//         int[] numbers = new int[n];
 
-        for(int i=0; i < n; i++)
-        {
-            Console.WriteLine("Lütfen {0} sayisini giriniz:", i+1);
-            numbers[i] = int.Parse(Console.ReadLine());
-        }
+//         for(int i=0; i < n; i++)
+//         {
+//             Console.WriteLine("Lütfen {0} sayisini giriniz:", i+1);
+//             numbers[i] = int.Parse(Console.ReadLine());
+//         }
+// Console.WriteLine("Lütfen bir sayı daha giriniz");
+//         int m = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Lütfen bir sayı daha giriniz");
-        int m = int.Parse(Console.ReadLine());
+//         for(int i=0; i<n; i++)
+//         {
+//             if(numbers[i] == m || numbers[i] % m == 0)
+//             {
+//                 Console.WriteLine(numbers[i]);
+//             }
+//         }
+        Student student = new Student(1, "onur");
 
-        for(int i=0; i<n; i++)
-        {
-            if(numbers[i] == m || numbers[i] % m == 0)
-            {
-                Console.WriteLine(numbers[i]);
-            }
-        }
+        Student onur;
 
+        AssignNameToStudent(out onur, "Onur");
 
+        Console.WriteLine(onur.Name);
+
+        
+    }
+
+    static void AssignNameToStudent(out Student student, string name)
+    {
+        student = new Student();
+        student.Name = name;
     }
 }
---
+++ Homeworks/equal-or-divisible/Student.cs
@@ -0,0 +1,15 @@
+public class Student
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+    
+    public Student(int id, string name)
+    {
+
+    }
+
+    public Student()
+    {
+    }
+}
